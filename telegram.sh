55e852af0406a805272f5e53d7f3f32a49d3148e
PattyMB <patriciaboreci@hotmail.com>
2022-09-26 17:35:15 -0300

    telegram notification

---
+++ telegram.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Get the token from Travis environment vars and build the bot URL:
+BOT_URL="https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage"
+
+PARSE_MODE="Markdown"
+
+
+echo steps.deploy.outcome
+
+
+# Define send message function. parse_mode can be changed to
+# HTML, depending on how you want to format your message:
+
+MESSAGE="
+-------------------------------------
+${{ github.actor }} created commit:
+Commit message: ${{ github.event.commits[0].message }}
+
+Repository: ${{ github.repository }}
+
+See changes: https://github.com/${{ github.repository }}/commit/${{github.sha}}
+--------------------------------------
+"
+
+# echo "${MESSAGE}"
+# echo "$MESSAGE"
+
+curl -s -X POST ${BOT_URL} -d chat_id=$TELEGRAM_CHAT_ID -d text="${MESSAGE}" -d parse_mode=${PARSE_MODE}
